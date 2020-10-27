522b3b436dbbeff905992e379496ec76ed380fb6
Aditya Bontha <bonthaaditya@gmail.com>
2020-10-27 17:44:49 +0400

    Peer-Peer Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+##These two below functions make computing inverse of matrix as 
+## they store it in cache in case the same inverse has to be
+## calculated and the whole process of inversion does not
+## need to take place
 
-## Write a short comment describing this function
+## makeCacheMatrix stores a matrix and caches its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes imverse of the matrix using the solve()
+## function which R provides
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("Getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
